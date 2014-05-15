dc5f5b8c6c1ec44327aa12df36d83fed502eeafa
Kim Burgestrand <kim@burgestrand.se>
2014-05-15 23:26:41 +0200

    Add a checkpoints as a way of keeping track of execution order in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,19 @@ require "pry"
 
 Thread.abort_on_exception = true
 
-RSpec.configure do
+module Checkpoints
+  Lock = Mutex.new
 
+  attr_reader :checkpoints
+
+  def checkpoint(name)
+    Lock.synchronize do
+      @checkpoints ||= []
+      @checkpoints << name
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include(Checkpoints)
 end
