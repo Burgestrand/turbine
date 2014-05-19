208c324ef8bbc12a92ec7a94057f9298426546c6
Jonas Nicklas and Kim Burgestrand <dev+jnicklas+burgestrand@elabs.se>
2014-05-19 08:58:06 +0200

    Ensure tests run for no longer than 5s each

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require "timeout"
 require "turbine"
 require "pry"
 
@@ -5,4 +6,10 @@ Thread.abort_on_exception = true
 
 Dir["#{__dir__}/support/*.rb"].each do |file|
   require file
+end
+
+RSpec.configure do |config|
+  config.around(:each) do |example|
+    Timeout.timeout(5, TimeoutError, &example)
+  end
 end
